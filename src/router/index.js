c208535656224daa97318a06d397a7f88e5431da
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-07 15:07:11 +0900

    トップページのルーティング設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
-  ]
+    {
+      // トップページの設定
+      path: '/',
+      name: 'Home',
+      component: App,
+    },
 })
 
 export default router
