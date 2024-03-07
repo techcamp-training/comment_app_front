0dd9656a4a534312b7ecb106912656e30656c1bf
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-07 14:19:56 +0900

    コンポーネントの作成

---
+++ src/components/Chat.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="chat_card">
+    <h2 class="chat_title">タイトル</h2>
+    <p class="chat_content">つぶやき</p>
+  </div>
+</template>
+
+<style scoped>
+.chat_card {
+  border: 1px solid lightgray;
+  padding: 8px;
+}
+.chat_title {
+  margin: 8px 0;
+}
+.chat_content {
+  margin: 0;
+}
+</style>
