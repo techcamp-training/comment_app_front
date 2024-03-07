86afc80cfadb124af7c822720e9d5a19de3ea49f
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-07 18:39:52 +0900

    削除機能の実装

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,15 +1,24 @@
 <script setup>
+import { useCommentStore } from '@/stores/comment';
 const props = defineProps({
   comment: Object
 })
 
+const commentStore = useCommentStore();
 
+const deleteComment = async(commentId) => {
+  try{
+    await commentStore.removeComment(commentId);
+  }catch(error){
+    console.log("コメントの削除に失敗しました")
+  }
+}
 </script>
 
 <template>
   <div class="comment_wrapper">
     <p class="comment_content">{{ comment.content }}</p>
-    <button class="btn">削除</button>
+    <button class="btn" @click="deleteComment(comment)">削除</button>
   </div>
 </template>
 
